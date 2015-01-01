b705b6c90238e013021d8f7c8e368da50ce5012e
JadeBoumaza <jade.boumaza@laposte.net>
2015-01-01 17:26:54 +0100

    Update ProfilForm.java

--- ProfilForm.java
+++ ProfilForm.java
@@ -22,23 +22,26 @@ public class ProfilForm {
 	private static final String CHAMP_NUMTEL = "numTel";
 	private static final String CHAMP_DATENAISSANCE ="dateNaissance";
 	private static final String CHAMP_ADRESSE = "adresse";
+	private static final String CHAMP_MAILPRO = "mailPro";
 	// A FAIRE : Collections de Domaines de compétences, de loisirs, de langues
 		
 	public InfosCV remplirInfosCV(HttpServletRequest request) {
 		
 		String nom = getValeurChamp(request, CHAMP_NOM);
 		String prenom = getValeurChamp(request, CHAMP_PRENOM);
-		//String numTel = getValeurChamp(request, CHAMP_NUMTEL);
-		//String dateNaissance = getValeurChamp(request,CHAMP_DATENAISSANCE);
-		//String adresse = getValeurChamp(request,CHAMP_ADRESSE);
+		String numTel = getValeurChamp(request, CHAMP_NUMTEL);
+		String dateNaissance = getValeurChamp(request,CHAMP_DATENAISSANCE);
+		String adresse = getValeurChamp(request,CHAMP_ADRESSE);
+		String mailPro = getValeurChamp(request,CHAMP_MAILPRO);
 		
 		InfosCV infosCV = new InfosCV();
 		
 		infosCV.setNom(nom);
 		infosCV.setPrenom(prenom);
-		//infosCV.setNumTel(numTel);
-		//infosCV.setDateNaissance(dateNaissance);
-		//infosCV.setAdresse(adresse);
+		infosCV.setNumTel(numTel);
+		infosCV.setDateNaissance(dateNaissance);
+		infosCV.setAdresse(adresse);
+		infosCV.setMailPro(mailPro);
 		
 		return infosCV;		
 		
